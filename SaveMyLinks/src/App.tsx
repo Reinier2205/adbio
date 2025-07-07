e029626b1d7cc6782d54e330dc16ba816eb7b451
Reinier2205 <reinier.olivier@gmail.com>
2025-07-07 15:24:23 +0200

    Update App.tsx

--- SaveMyLinks/src/App.tsx
+++ SaveMyLinks/src/App.tsx
@@ -53,8 +53,8 @@ function AppContent() {
       <Header />
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="space-y-6">
-          <AddLinkForm />
           <SearchAndFilters />
+          <AddLinkForm />
           <LinkList />
         </div>
       </main>
