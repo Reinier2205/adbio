ddcbbadf722f510f5312cdad22cb78562f4d2dee
Reinier2205 <196901241+Reinier2205@users.noreply.github.com>
2026-07-03 16:08:52 +0200

    del sml uit adbio

--- SaveMyLinks/src/utils/idGenerator.ts
+++
@@ -1,7 +0,0 @@
-export const generateId = (): string => {
-  return crypto.randomUUID();
-};
-
-export const generateShareId = (): string => {
-  return Math.random().toString(36).substr(2, 6);
-}; 
